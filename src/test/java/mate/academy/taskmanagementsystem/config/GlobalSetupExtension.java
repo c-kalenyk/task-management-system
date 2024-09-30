8f09556c4742fc755b8d5228e00cd1c37b933679
Oleksandr Kalenyk <carbonmail88@gmail.com>
2024-09-30 12:32:47 +0300

    CI fix

--- src/test/java/mate/academy/taskmanagementsystem/config/GlobalSetupExtension.java
+++ src/test/java/mate/academy/taskmanagementsystem/config/GlobalSetupExtension.java
@@ -9,13 +9,11 @@ public class GlobalSetupExtension implements BeforeAllCallback {
     public void beforeAll(ExtensionContext extensionContext) throws Exception {
         if (System.getenv("CI") == null) {
             Dotenv dotenv = Dotenv.configure().load();
-            System.setProperty("MYSQL_ROOT_PASSWORD", dotenv.get("MYSQL_ROOT_PASSWORD"));
             System.setProperty("SPRING_MAIL_USERNAME", dotenv.get("SPRING_MAIL_USERNAME"));
             System.setProperty("SPRING_MAIL_PASSWORD", dotenv.get("SPRING_MAIL_PASSWORD"));
             System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
             System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
         } else {
-            System.setProperty("MYSQL_ROOT_PASSWORD", System.getenv("MYSQL_ROOT_PASSWORD"));
             System.setProperty("SPRING_MAIL_USERNAME", System.getenv("SPRING_MAIL_USERNAME"));
             System.setProperty("SPRING_MAIL_PASSWORD", System.getenv("SPRING_MAIL_PASSWORD"));
             System.setProperty("JWT_SECRET", System.getenv("JWT_SECRET"));
